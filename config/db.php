58d26b0e29e5cc483576cf03b69164e7c5d82a98
TuTu <tutu@10-18-173-40.dynapool.wireless.nyu.edu>
2025-05-07 11:45:54 -0400

    Update db_connection

--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
-$db_name = "jpn_pal";
+$db_name = "JPN_PAL";
 $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
